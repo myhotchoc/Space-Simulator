6f09ec85bd5eadfd5a1f46a3dc8eb6942c008ca4
Danny Berlin <dannyberlin2@gmail.com>
2020-06-07 18:01:34 +0100

    Update Pygame Environmetnt.py

--- Pygame Environmetnt.py
+++ Pygame Environmetnt.py
@@ -14,13 +14,24 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255,255,0)
+BLUE = (0, 0, 255)
+PURPLE = (238, 130, 238)
 
 class Planet(object):
-    def __init__(self, radius, orbital_dist, colour, speed):
+    def __init__(self, radius, orbital_dist, colour, speed, window):
         self.radius = radius
         self.orbital_dist = orbital_dist
         self.colour = colour
         self.speed = speed
+        self.window = window
+        
+        #pygame.draw.circle(scree)
+    
+    def placePlanet(self):
+        
+        planet_pos = (centre[0], centre[1] - self.orbital_dist)
+        
+        pygame.draw.circle(self.window, self.colour, planet_pos, self.radius)
 
 # Window size
 win_size = (1000, 1000)
@@ -30,7 +41,6 @@ screen = pygame.display.set_mode(win_size)
 
 # Defining centre of screen
 centre = (int(win_size[0]/2), int(win_size[1]/2))
-pos = centre
 
 #Initialise window
 pygame.init()
@@ -48,16 +58,31 @@ while game_loop:
         if item.type == pygame.QUIT:
             game_loop = False
         
-        #if item.type == pygame.MOUSEBUTTONUP:
-            #pos = (random.randint(0, win_size[0]), random.randint(0, win_size[1]))
-        #soz but this isn't needed becuase the sun doesn't move
+        if item.type == pygame.MOUSEBUTTONUP:
+            p1 = Planet(50, 1, RED, 10, screen)
+            p1.placePlanet()
+        
             
     # Black background, white circle in centre
     screen.fill(BLACK)
-    circle1 = pygame.draw.circle(screen, YELLOW, pos, 40)
+    
+    ## (win, col, pos, r)
+    sun = pygame.draw.circle(screen, YELLOW, centre, 40)
+    
+    p1 = Planet(10, 60, RED, 10, screen)
+    p1.placePlanet()
+    
+    p2 = Planet(15, 120, GREEN, 10, screen)
+    p2.placePlanet()
+    
+    p3 = Planet(10, 190, BLUE, 10, screen)
+    p3.placePlanet()
+    
+    p4 = Planet(20, 250, PURPLE, 10, screen)
+    p4.placePlanet()
 
     
-    ellipse1 = pygame.draw.ellipse(screen, WHITE, [100,100,800,300], 3)
+    #ellipse1 = pygame.draw.ellipse(screen, WHITE, [100,100,800,300], 3)
     
     # Update Screen, set framerate
     pygame.display.flip()
