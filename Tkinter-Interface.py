d35bd0277aafd91643d02a638438918af3713d29
Danny Berlin <dannyberlin2@gmail.com>
2020-06-07 20:02:23 +0100

    Update Tkinter-Interface.py

--- Tkinter-Interface.py
+++ Tkinter-Interface.py
@@ -21,10 +21,17 @@ planets_array = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uran
 # merc = tk. Radiobutton(root, text= planets_array[0], variable=v).pack()
 # print(merc)
 planets_selection = tk.Listbox(root, selectmode = 'multiple', height = 8 )
+
 for item in planets_array:
     planets_selection.insert(tk.END, item)
-planets_selection.pack()
+    
+
 
 array = planets_selection.get(tk.ACTIVE)
+planets_selection.pack()
+
+btn = tk.Button(root, text='print', command=lambda: print (planets_selection.get(tk.ACTIVE)))
+btn.pack()
+
 print(array)
 root.mainloop()
