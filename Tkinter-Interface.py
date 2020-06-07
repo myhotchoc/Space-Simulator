d735c77ef0e2f246cbb9f66f56a45baaf2a1e08c
jennimm <jennifermoulder17@gmail.com>
2020-06-07 20:22:04 +0100

    Tidying up code

--- Tkinter-Interface.py
+++ Tkinter-Interface.py
@@ -6,7 +6,6 @@ Created on Sun Jun  7 17:29:45 2020
 """
 
 import tkinter as tk
-from tkinter import ttk
 
 root = tk.Tk()
 v = tk.StringVar()
---
+++ Tkinter-Planet-Info.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  7 17:29:45 2020
+
+@author: Danny and Jenni (but he smells)
+"""
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
