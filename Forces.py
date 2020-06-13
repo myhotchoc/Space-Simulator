acd7e3d1c7f710d54f103e7ddef74109ef953d08
Danny Berlin <dannyberlin2@gmail.com>
2020-06-13 06:23:01 +0100

    Update Forces.py

--- Forces.py
+++ Forces.py
@@ -7,10 +7,30 @@ Created on Fri Jun 12 07:51:52 2020
 
 import pygame
 import math
+import random
 
 ## Hold mouse button to stop gravity
 ## Press left or right arrow keys to add wind to left or right
 
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 128, 0)
+YELLOW = (242, 224, 63)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+g = 1
+mu = 0.1
+
+vel_limit = 20
+
+windLeftVector = [-75 ,0]
+windRightVector = [75, 0]
+
+gravity = False
+windRight = False
+windLeft = False
 
 ## Setting window size, defining screen
 win_size = (900, 900)
@@ -50,53 +70,110 @@ def setMag(x, y, m):
     
     return x, y
 
+def unitVector(x, y):
+    x, y = setMag(x, y, 1)
+    
+    return x, y
+
 def limit(x, y, a):
     if mag(x, y) >= a:
         x, y = setMag(x, y, a)
         
     return x, y
 
-px = centre[0]
-py = centre[1]
+def drawArrow(screen, colour, start, end):
+    pygame.draw.line(screen,colour,start,end,2)
+    rotation = math.degrees(math.atan2(start[1]-end[1], end[0]-start[0]))+90
+    pygame.draw.polygon(screen, colour, ((end[0]+20*math.sin(math.radians(rotation)), end[1]+20*math.cos(math.radians(rotation))), (end[0]+20*math.sin(math.radians(rotation-120)), end[1]+20*math.cos(math.radians(rotation-120))), (end[0]+20*math.sin(math.radians(rotation+120)), end[1]+20*math.cos(math.radians(rotation+120)))))
 
-vx = 0
-vy = 0
-
-ax = 0
-ay = 0
-
-acc_val = 0.5
-vel_limit = 20
-
-gravity = True
-windRight = False
-windLeft = False
-
-def changePos(px, py, vx, vy):
+class Body(object):
+    def __init__(self, col, mass, p, v, a, f):
+        self.col = col
+        self.mass = mass
+        self.radius = int(math.sqrt(mass) * 3)
+        
+        self.px = p[0]
+        self.py = p[1]
+        
+        self.vx = v[0]
+        self.vy = v[1]
+        
+        self.ax = a[0]
+        self.ay = a[1]
+        
+        self.fx = f[0]
+        self.fy = f[1]
+        
+        self.weight = [0, self.mass * g]
     
-    px += vx
-    py += vy
+    def changePos(self):
+        
+        self.px = int(self.px + self.vx)
+        self.py = int(self.py + self.vy)
     
-    return px, py
+    def changeVel(self):
+        self.vx += self.ax
+        self.vy += self.ay
+        
+    def changeAcc(self):
+        self.ax += self.fx/self.mass
+        self.ay += self.fy/self.mass
+        
+    def applyForce(self, force):
+        self.fx += force[0]
+        self.fy += force[1]
+        
+        drawArrow(screen, (100, 50, 231), (self.px, self.py),(self.px+self.fx, self.py+self.fy))
+        
+    def friction(self):
+        
+        unit_vx, unit_vy = unitVector(-self.vx, -self.vy)
+        N = self.mass
+        
+        fric_x = unit_vx * mu * N
+        fric_y = unit_vy * mu * N
+        
+        self.applyForce((fric_x, fric_y))
+        
+    
+#Body(col, mass, radius, p, v, a, f)
+body1 = Body(RED, 50, (win_size[0]/4, centre[1] - int((math.sqrt(50) * 3))),
+             (0, 0),
+             (0, 0),
+             (0, 0))
 
-def changeVel(vx, vy, ax, ay):
-    vx += ax
-    vy += ay
-    
-    return vx, vy
+body2 = Body(BLUE, 100, (win_size[0]/2, win_size[1] - int((math.sqrt(100) * 3))),
+             (0, 0),
+             (0, 0),
+             (0, 0))
 
-def changeAcc(ax, ay, fx, fy):
-    ax += fx
-    ay += fy
-    
-    return ax, ay
+body3 = Body(GREEN, 200, (3*win_size[0]/4, win_size[1] - int((math.sqrt(200) * 3))),
+             (0, 0),
+             (0, 0),
+             (0, 0))
+
+bodies = [body1, body2, body3]
+
+bodies = [body1]
+bodies = []
+
+for x in range(1, 11):
+    body = Body((random.randint(0, 255), random.randint(0,255), random.randint(0, 255)),
+                25*x,
+                (x * win_size[0]/10, centre[1]),
+                (random.randint(-10,10), random.randint(-10,10)), 
+                (0, 0),
+                (0, 0))
+    bodies.append(body)
+
+
 
 
 while win_loop:
     ## Refills background
     screen.fill((0, 0, 0))
     
-    fx, fy = 0, 0
+
     
     ## Checks for QUIT event to break loop
     for i in pygame.event.get():
@@ -121,42 +198,39 @@ while win_loop:
             if i.key == pygame.K_LEFT:
                 windLeft = False
     
-    mouse = pygame.mouse.get_pos()
-    
-    if gravity == True:
-        fy += 0.5
-    
-    if windRight == True:
-        fx += 0.1
-    
-    if windLeft == True:
-        fx -= 0.1
-    
-    accx, accy = changeAcc(ax, ay, fx, fy)
-    
-    accx, accy = setMag(accx, accy, acc_val)
-    
-    vx, vy = changeVel(vx, vy, accx, accy)
-    
-    print (limit(vx, vy, vel_limit))
-    
-    vx, vy = limit(vx, vy, vel_limit)
-    
-    
-    if px+vx >= win_size[0] or px+vx < 0:
-        vx = -vx
+    for i in bodies:
         
-    if py+vy >= win_size[1] or py+vy < 0:
-        vy = -vy
+        i.ax, i.ay = 0, 0
+        i.fx, i.fy = 0, 0
     
-    px, py = changePos(px, py, vx, vy)
-    
-    if px >= win_size[0] or px < 0 or py >= win_size[1] or py <= 0:
-        px, py = px, py
+        if gravity == True:
+            i.applyForce(i.weight)
         
-    pygame.draw.circle(screen, (255, 100, 0), (int(px), int(py)), 20)
+        if windRight == True:
+            i.applyForce(windRightVector)
+        
+        if windLeft == True:
+            i.applyForce(windLeftVector)
+        
+        i.friction()
+        
+        i.changeAcc()
+        #i.ax, i.ay = setMag(i.ax, i.ay, 0.5)
+        i.changeVel()
+        
+        i.vx, i.vy = limit(i.vx, i.vy, vel_limit)
+        
+        
+        if i.px + i.vx + i.radius >= win_size[0] or i.px + i.vx - i.radius <= 0:
+            i.vx = -i.vx
+            
+        if i.py + i.vy + i.radius >= win_size[1] or i.py + i.vy - i.radius <= 0:
+            i.vy = -i.vy
+        
+        i.changePos()
+            
+        pygame.draw.circle(screen, i.col, (i.px, i.py), i.radius)
 
-    
     ## Update screen
     pygame.display.flip()
     clock.tick(60)
